765f2f3d40550e2a84c3904c2c30585842bd4cb8
Roni <roninemat@gmail.com>
2014-12-07 19:22:47 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,23 @@
+
+# Here we will first convert the text file into a csv file and name it power.csv
+p<-read.csv("power.csv", header = TRUE)
+ 
+# This is to make sure the data is correct
+head(p)
+
+# Below we will paste together the date and time column so that it reads it as one column
+p$datetime<-paste(p$Date, p$Time)
+ head(p$datetime)
+
+
+#Formating the datetime column
+ p$posixDate <- strptime(p$datetime, "%d/%m/%Y %H:%M:%S")
+ 
+ 
+# Plot chart with correct labels
+ plot(p$posixDate,p$Global_active_power,type="l", ylab="Global Active Power (killowatts)", xlab= "")
+ 
+#conert to png file 
+dev.copy(png, file="plot2.png", height = 480, width =480)
+
+ dev.off()
