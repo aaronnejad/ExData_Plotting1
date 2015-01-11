12308e93c0b23f867a72dfcf6f53d88af8c77fab
Roni <roninemat@gmail.com>
2015-01-11 18:13:41 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,10 @@
 #First need to export the textfile data into an Excel cvs file called power.csv. I then subsetted the correct columns and rows.I actually subsetted using Excel methods. 
+#Here I saved the text file in /Desktop/Coursera/Explore
+
+#I open Excel and then open the textfile from Excel, I comma delimit and then the Excel csv file has all the columns correctly allinged (if I delimit it using the Excel tools)
+#I save the Excel csv file as    "power.csv"
+#For the first two plots I converted the files direct into csv as they have fewer columns. For plot 3 and 4 I use 
+#read.csv.sql
 
 p<-read.csv("power.csv",header=TRUE)
 
