797d5b0f7c47077910805cba04dfda34f5bf40ef
Roni <roninemat@gmail.com>
2015-02-08 19:08:26 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,11 +1,13 @@
 
-# Here we will first convert the text file into a csv file and name it "power.csv". I already subsetted the data using Excel methods. Please see instructions for plot1.R 
-# for further explanation
+
 #In folder Desktop/Coursera/Explore should find a csv file called "power.csv"
 # as explained in previous R file. Subsetting couldve been done using R's subsetting methods however for this simple
-# section where only 2 variables are used I used Excel Csv's subsetting methods. For plot 3 and 4 I used read.csv.sql
+# section where only 2 variables are used I used Excel Csv's and sql subsetting methods. 
 
-p<-read.csv("power.csv", header = TRUE)
+p <- read.csv.sql("household_power_consumption.txt", header = TRUE, sep = ";", 
+colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), 
+sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
+
  
 # This is to make sure the data is correct
 head(p)
@@ -22,7 +24,7 @@ p$datetime<-paste(p$Date, p$Time)
 # Plot chart with correct labels
  plot(p$posixDate,p$Global_active_power,type="l", ylab="Global Active Power (killowatts)", xlab= "")
  
-#conert to png file 
+#connect to png file 
 dev.copy(png, file="plot2.png", height = 480, width =480)
 
  dev.off()
